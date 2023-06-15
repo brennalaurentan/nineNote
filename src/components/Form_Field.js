5baf7c02632a79dde40a0c788fcc1d8483c72c49
brennalaurentan <122245137+brennalaurentan@users.noreply.github.com>
2023-06-15 17:21:47 +0800

    updated FormField.js

--- src/components/Form_Field.js
+++ src/components/Form_Field.js
@@ -1,10 +1,17 @@
 import { TextField, MenuItem } from '@mui/material';
 
+<<<<<<< Updated upstream:src/components/Form_Field.js
 const normal_field = (field_name) => <TextField
   id="outlined-search"
   label={field_name}
   type="search"
 />
+=======
+// components / pages / images
+
+// tools
+import { TextField, MenuItem } from '@mui/material';
+>>>>>>> Stashed changes:src/components/common/FormField.js
 
 const email_field = (field_name, onChangeAction) => < TextField
   id="outlined-search"
@@ -21,10 +28,20 @@ const password_field = (field_name, onChangeAction) => <TextField
   onChange={onChangeAction}
 />
 
+<<<<<<< Updated upstream:src/components/Form_Field.js
 const dropdown_field = (field_name, values) => <TextField
   id="outlined-select-currency"
   select
   label={field_name}
+=======
+const dropdown_field = (field_name, values, onChangeAction) =>
+<TextField
+  id="outlined-select-currency"
+  select
+  label={field_name}
+  defaultValue={''}
+  onChange={onChangeAction}
+>>>>>>> Stashed changes:src/components/common/FormField.js
 >
   {values.map((option) => (
     <MenuItem key={option.value} value={option.value}>
@@ -40,7 +57,7 @@ const Form_Field = ({ field_name, type, values, onChangeAction }) => {
       ? password_field(field_name, onChangeAction)
       : type == "email"
         ? email_field(field_name, onChangeAction)
-        : dropdown_field(field_name, values);
+        : dropdown_field(field_name, values, onChangeAction);
 }
 
 export default Form_Field
