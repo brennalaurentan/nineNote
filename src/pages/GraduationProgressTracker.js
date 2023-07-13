1b9505cdf4aaafb8f656be1625f87d86cb826fbc
brennalaurentan <122245137+brennalaurentan@users.noreply.github.com>
2023-07-13 12:19:19 +0800

    updated GraduationProgressTracker.js
    connected progress rings to data in database

--- src/pages/GraduationProgressTracker.js
+++ src/pages/GraduationProgressTracker.js
@@ -10,47 +10,82 @@ import ModulePlanner from '../components/graduation_progress_tracker/ModulePlann
 import SnackBar from '../components/common/SnackBar';
 
 // tools
+import * as React from 'react';
 import { Helmet } from 'react-helmet';
 import { Stack } from '@mui/material';
+import { getAuth } from 'firebase/auth';
+import { collection, getDocs } from 'firebase/firestore';
+import { db } from '../components/others/firebase';
+import { useEffect } from 'react';
 
 const GraduationProgressTracker = () => {
+
+  const auth = getAuth();
+  const user = auth.currentUser;
+  const currentUserEmail = user.email;
+
   // edits to the variables below will be reflected in the progress rings
-  let ccr = 8;
+  //let ccr = 8;
   let ccrTotal = 40;
-  let pr = 24;
+  //let pr = 24;
   let prTotal = 80;
-  let uer = 0;
+  //let uer = 0;
   let uerTotal = 40;
+
+  const [ccr, setCCR] = React.useState();
+  const [pr, setPR] = React.useState();
+  const [uer, setUER] = React.useState();
+
+  async function retrieveProgressFields() {
+    const gradProgressCollectionPath = `users/${currentUserEmail}/gradProgress`;
+    const gradProgressCollection = collection(db, gradProgressCollectionPath);
+    const gradProgressQuerySnapshot = await getDocs(gradProgressCollection);
+    gradProgressQuerySnapshot.forEach((mainModuleGroup) => {
+      if (mainModuleGroup.id === "commonCurriculum") {
+        setCCR(mainModuleGroup.data().creditsCompleted);
+        console.log("ccr is " + ccr);
+      }
+      else if (mainModuleGroup.id === "programme") {
+        setPR(mainModuleGroup.data().creditsCompleted);
+        console.log("pr is " + pr);
+      }
+      else if (mainModuleGroup.id === "unrestrictedElectives") {
+        setUER(mainModuleGroup.data().creditsCompleted);
+        console.log("uer is " + uer);
+      }
+    });
+  }
+  retrieveProgressFields();
 
   // edits to the variables below will be reflected in the progress bar
   let totalNumerator = ccr + pr + uer;
   let totalDenominator = ccrTotal + prTotal + uerTotal;
 
-  return (
-    <>
-      <Helmet>
-        <title>nineNote | Graduation Progress Tracker</title>
-      </Helmet>
-      <MainNavbar />
-      <SnackBar type="success" text="You have logged in successfully." />
-      <Stack direction="row" gap="64px" padding="56px">
-        <Stack gap="56px">
-          <CreditsSelected
-            ccr={ccr}
-            ccrTotal={ccrTotal}
-            pr={pr}
-            prTotal={prTotal}
-            uer={uer}
-            uerTotal={uerTotal} />
-          <GraduationStatus
-            numerator={totalNumerator}
-            denominator={totalDenominator} />
-          <ModuleExemptions />
+    return (
+      <>
+        <Helmet>
+          <title>nineNote | Graduation Progress Tracker</title>
+        </Helmet>
+        <MainNavbar />
+        <SnackBar type="success" text="You have logged in successfully." />
+        <Stack direction="row" gap="64px" padding="56px">
+          <Stack gap="56px">
+            <CreditsSelected
+              ccr={ccr}
+              ccrTotal={ccrTotal}
+              pr={pr}
+              prTotal={prTotal}
+              uer={uer}
+              uerTotal={uerTotal} />
+            <GraduationStatus
+              numerator={totalNumerator}
+              denominator={totalDenominator} />
+            <ModuleExemptions />
+          </Stack>
+          <ModulePlanner />
         </Stack>
-        <ModulePlanner />
-      </Stack>
-    </>
-  )
+      </>
+    )
 }
 
 export default GraduationProgressTracker
