f8d37d6f0e5cab422961532820094a48ad5467eb
triciiaaa <angjeanee@gmail.com>
2023-07-13 18:26:04 +0800

    retrieved user's year and course from firebase and display in dropdown
    - there is an issue with onChange action

--- src/components/my_profile/BasicInfoForm.js
+++ src/components/my_profile/BasicInfoForm.js
@@ -5,12 +5,13 @@ import FormField from '../common/FormField';
 import MainButton from '../common/MainButton';
 
 // tools
-import { Stack, Link, Typography, MenuItem, TextField } from '@mui/material';
-import { auth } from '../others/firebase';
+import { Stack, Link, Typography, MenuItem, TextField, Box } from '@mui/material';
+import { auth, db } from '../others/firebase';
 import { onAuthStateChanged } from 'firebase/auth';
-import { useState } from 'react';
+import { getDoc, doc, collection } from 'firebase/firestore';
+import { useEffect, useState, useRef } from 'react';
 
-const matriculation_year = [
+const static_matriculation_year = [
   {
     value: '1',
     label: 'AY23/24',
@@ -23,25 +24,9 @@ const matriculation_year = [
     value: '3',
     label: 'AY21/22',
   },
-  {
-    value: '4',
-    label: 'AY20/21',
-  },
-  {
-    value: '5',
-    label: 'AY19/20',
-  },
-  {
-    value: '6',
-    label: 'AY18/19',
-  },
-  {
-    value: '7',
-    label: 'AY17/18',
-  },
 ];
 
-const course = [
+const static_course = [
   {
     value: 'CHS1',
     label: 'Data Science and Economics',
@@ -146,16 +131,161 @@ const BasicInfoForm = () => {
     setUser(currentUser);
   });
 
+  // handles updated data to firebase based on user's new inputs
+  const [defaultMatriculationYearValue, setDefaultMatriculationYearValue] = useState('');
+  const [matriculationYearArray, setMatriculationYearArray] = useState([]);
+  const matriculationYearLabelList = matriculationYearArray.map(year => year.label);
+  const [matriculationYear, setMatriculationYear] = useState("");
+
+  const [defaultCourseValue, setDefaultCourseValue] = useState('');
+  const [courseArray, setCourseArray] = useState([]);
+  const courseLabelList = courseArray.map(course => course.label);
+  const courseValueList = courseArray.map(course => course.value);
+
+  // function to load all matriculation years from firebase (code section below is commented to avoid firebase quota)
+  useEffect(() => {
+    let matriculationYearArray = [];
+    const matriculationYearCollectionRef = collection(db, "matriculationYear");
+    async function loadMatriculationYearList() {
+      try {
+        // code below reads all the matriculation years from database and
+        // displays each of them in the matriculation year dropdown list
+        /*
+        const qSnapshot = getDocs(matriculationYearCollectionRef)
+          .then((qSnapshot) => {
+
+            console.log("matriculationYear qSnapshot: " + qSnapshot);
+            qSnapshot.forEach(childDoc => {
+              let newElement = {
+                "value": matriculationYearArray.length.toString(),
+                "label": childDoc.data().year
+              }
+              console.log("pushing label: " + childDoc.data().year);
+              matriculationYearArray.push(newElement);
+            })
+            console.log("matriculationYearArray: " + matriculationYearArray.toString());
+            matriculationYearArray.forEach((item) => console.log(item));
+            console.log("staticMatriculationyear: " + static_matriculation_year.toString());
+            static_matriculation_year.forEach((item) => console.log(item));
+            setMatriculationYearArray(matriculationYearArray);
+          });
+        */
+        setMatriculationYearArray(static_matriculation_year);
+      } catch (error) {
+        console.log(error.message);
+      }
+    }
+    loadMatriculationYearList();
+  }, []);
+
+  // function to load all courses from firebase (code section below is commented to avoid firebase quota)
+  useEffect(() => {
+    let courseArray = [];
+    /*
+    const courseCollectionRef = collection(db, "courseLibrary");
+    */
+    async function loadCourseList() {
+      try {
+        /*
+        const qSnapshot = getDocs(courseCollectionRef)
+          .then((qSnapshot) => {
+
+            console.log("course qSnapshot: " + qSnapshot);
+            // for each faculty in courseLibrary (faculty is a document)
+            qSnapshot.forEach(async faculty => {
+              let facultyCourseCount = 0;
+              const courseSnapshot = await getDocs(collection(db, `courseLibrary/${faculty.id}/courses`));
+              // for each course in the childDoc faculty (course is a document)
+              courseSnapshot.forEach(course => {
+                facultyCourseCount++;
+                let newElement = {
+                  "value": faculty.id.toString() + facultyCourseCount.toString(),
+                  "label": course.id.toString()
+                }
+                console.log("pushing value: " + faculty.id.toString() + facultyCourseCount.toString());
+                console.log("pushing label: " + course.id.toString());
+                console.log("pushing label id: " + course.id);
+                console.log("current facultycoursecount is " + facultyCourseCount.toString());
+                console.log(courseArray);
+                courseArray.push(newElement);
+                setCourseArray(courseArray);
+              })
+            })
+            console.log("courseArray: " + courseArray.toString());
+            courseArray.forEach((item) => console.log(item));
+            console.log("staticCourse: " + static_course.toString());
+            static_course.forEach((item) => console.log(item));
+        });
+        */
+        // code below adds a single 'Computer Science' course to the course dropdown
+        let newElement = {
+          "value": "SOC1",
+          "label": "Computer Science"
+        }
+        courseArray.push(newElement);
+        setCourseArray(courseArray);
+      } catch (error) {
+        console.log(error.message);
+      }
+    }
+    loadCourseList();
+  }, []);
+
+  // local variable of user's matriculation year retrieved from firebase
+  const retrievedMatriculationYear = useRef("");
+
+  // local variable of user's course retrieved from firebase
+  const retrievedCourse = useRef("");
+
+  // function to retrieve user's matriculation year from firebase and update state
+  useEffect(() => {
+    async function retrieveUserMatriculationYear() {
+      const docRef = doc(db, "users", `${user.email}`);
+      const docSnap = await getDoc(docRef);
+      try {
+        const userData = docSnap.data();
+        retrievedMatriculationYear.current = userData['matriculationYear'];
+        const value = matriculationYearLabelList.indexOf(retrievedMatriculationYear.current) + 1;
+        setDefaultMatriculationYearValue(value);
+        console.log("retrieved data: ", userData);
+        console.log("year retrieved.current: ", retrievedMatriculationYear.current);
+        console.log("year value: ", value);
+      } catch (error) {
+        console.log(error.message);
+      }
+    }
+    retrieveUserMatriculationYear();
+  })
+
+  // function to retrieve user's course from firebase and update state
+  useEffect(() => {
+    async function retrieveUserCourse() {
+      const docRef = doc(db, "users", `${user.email}`);
+      const docSnap = await getDoc(docRef);
+      try {
+        const userData = docSnap.data();
+        retrievedCourse.current = userData['course'];
+        const value = courseValueList[courseLabelList.indexOf(retrievedCourse.current)];
+        setDefaultCourseValue(value);
+        console.log("retrieved data: ", userData);
+        console.log("course retrieved.current: ", retrievedCourse.current);
+        console.log("course value: ", value);
+      } catch (error) {
+        console.log(error.message);
+      }
+    }
+    retrieveUserCourse();
+  })
+
+  const handleDropdownChange = (event) => {
+    console.log("selected year value: ", event.target.value);
+    setDefaultMatriculationYearValue(event.target.value);
+  }
+
+  // function to save user changes and update data in firebase
   const saveChanges = () => {
     console.log("changes saved!");
   }
-
-  // update data retrieval from static to firebase
-  const [currentMatriculationYear, setCurrentMatriculationYear] = useState('AY19/20'); // update with firebase current user state
-  const matriculationYearLabelList = matriculation_year.map(year => year.label);
-  const [currentCourse, setCurrentCourse] = useState('Computer Science'); // update with firebase current user state
-  const courseLabelList = course.map(course => course.label);
-  const courseValueList = course.map(course => course.value);
 
   return (
     <>
@@ -185,25 +315,20 @@ const BasicInfoForm = () => {
         <FormField
           field_name={"Matriculation Year"}
           type={"dropdown"}
-          values={matriculation_year}
-          defaultValue={matriculationYearLabelList.indexOf(currentMatriculationYear) + 1}
-          onChangeAction={(event) => {
-            // amend once update with dropdown field values retrieval from firebase
-            console.log(matriculationYearLabelList.indexOf(currentMatriculationYear));
-          }}
+          values={matriculationYearArray}
+          value={defaultMatriculationYearValue}
+        // onChangeAction={event => setDefaultMatriculationYearValue(event.target.value)}
         />
 
         {/* current/prospective course */}
         <FormField
           field_name={"Current/Prospective Course"}
           type={"dropdown"}
-          values={course}
-          defaultValue={courseValueList[courseLabelList.indexOf(currentCourse)]}
-          onChangeAction={(event) => {
-            // amend once update with dropdown field values retrieval from firebase
-            console.log(courseValueList[courseLabelList.indexOf(currentCourse)]);
-          }}
+          values={courseArray}
+          value={defaultCourseValue}
+        // onChangeAction={(event) => {}}
         />
+
         <Link>
           <MainButton
             type="contained"
