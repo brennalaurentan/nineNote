fc29152df1540158880cd04c4e840e36c5a04471
triciiaaa <angjeanee@gmail.com>
2023-07-21 12:00:35 +0800

    updated module exemptions 'add' and 'delete' functionality

--- src/components/graduation_progress_tracker/ModuleExemptions.js
+++ src/components/graduation_progress_tracker/ModuleExemptions.js
@@ -5,12 +5,15 @@ import ModulePill from './ModulePill';
 import ButtonDialog from './ButtonDialog';
 
 // tools
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import { Typography, Box, Stack } from '@mui/material';
 import { DragDropContext, Droppable, Draggable } from 'react-beautiful-dnd';
 import _ from "lodash";
 import { v4 } from 'uuid';
-import CreditCount from './CreditCount';
+import { auth, db } from '../others/firebase';
+import { collection, getDocs } from 'firebase/firestore';
+import { onAuthStateChanged } from 'firebase/auth';
+// import CreditCount from './CreditCount';
 
 const item1 = {
     id: v4(),
@@ -20,12 +23,53 @@ const item1 = {
 }
 
 const ModuleExemptions = () => {
-    const [state, setState] = useState({
-        "Exemptions": {
-            title: "",
-            items: [item1]
+
+    // handles currently signed-in user
+    const [user, setUser] = useState({});
+
+    // function to get the currently signed-in user
+    useEffect(() => {
+        onAuthStateChanged(auth, (currentUser) => {
+            setUser(currentUser);
+        });
+    }, [])
+
+    const [moduleExemptions, setModuleExemptions] = useState({});
+    
+    useEffect(() => {
+        async function loadModuleExemptions() {
+            const allModulesCollectionRef = collection(db, `users/${user.email}/modules`);
+            const allModulesSnapshot = await getDocs(allModulesCollectionRef);
+
+            try {
+                let exemptionMods = [];
+                allModulesSnapshot.forEach(module => {
+                    const newItem = {
+                        moduleID: module.data().moduleID,
+                        moduleCode: module.data().moduleCode,
+                        moduleName: module.data().moduleName,
+                        moduleMC: module.data().moduleMC,
+                        moduleCategory: module.data().moduleCategory
+                    }
+
+                    exemptionMods.push(newItem);
+                })
+
+
+                let newState = {
+                    "Exemptions": { title: "", items: exemptionMods },
+                };
+
+                setModuleExemptions(newState);
+
+                console.log("new state: ", newState);
+
+            } catch (error) {
+                console.log(error.message);
+            }
         }
-    })
+        loadModuleExemptions();
+    }, [user]);
 
     const handleDragEnd = ({ destination, source }) => {
         console.log("from", source)
@@ -40,8 +84,8 @@ const ModuleExemptions = () => {
         }
 
         // creating a copy of item before removing from state
-        const itemCopy = { ...state[source.droppableId].items[source.index] };
-        setState(prev => {
+        const itemCopy = { ...moduleExemptions[source.droppableId].items[source.index] };
+        setModuleExemptions(prev => {
             prev = { ...prev }
             // remove from previous items array
             prev[source.droppableId].items.splice(source.index, 1)
@@ -54,17 +98,17 @@ const ModuleExemptions = () => {
     const addModule = (moduleCode, moduleName, moduleMC, yearSem) => {
         console.log("Module Added: " + moduleCode);
         console.log("Year and Semester: " + yearSem);
-        setState(prev => {
+        setModuleExemptions(prev => {
             return {
                 ...prev,
                 [yearSem]: {
                     title: "",
                     items: [
                         {
-                            id: v4(),
-                            code: moduleCode,
-                            name: moduleName,
-                            mc: moduleMC
+                            moduleID: v4(),
+                            moduleCode: moduleCode,
+                            moduleName: moduleName,
+                            moduleMC: moduleMC
                         },
                         ...prev[yearSem].items
                     ]
@@ -76,13 +120,13 @@ const ModuleExemptions = () => {
     const deleteModule = (moduleID, moduleCode, yearSem) => {
         console.log("Module Deleted: " + moduleCode);
         console.log("Year and Semester: " + yearSem);
-        setState(prev => {
+        setModuleExemptions(prev => {
             return {
                 ...prev,
                 [yearSem]: {
                     title: "",
                     items: [
-                        ...prev[yearSem].items.filter(module => module.id !== moduleID)
+                        ...prev[yearSem].items.filter(module => module.moduleID !== moduleID)
                     ]
                 }
             }
@@ -99,7 +143,7 @@ const ModuleExemptions = () => {
         <Stack gap="32px">
             <Typography variant="h3">Module Exemptions</Typography>
             <DragDropContext onDragEnd={handleDragEnd}>
-                {_.map(state, (data, key) => {
+                {_.map(moduleExemptions, (data, key) => {
                     return (
                         <>
                             <Box bgcolor="light_blue.light" padding="30px" borderRadius="30px" display="flex" flexDirection="column" gap="30px">
@@ -123,7 +167,7 @@ const ModuleExemptions = () => {
                                             >
                                                 {data.items.map((el, index) => {
                                                     return (
-                                                        <Draggable key={el.id} index={index} draggableId={el.id}>
+                                                        <Draggable key={el.moduleID} index={index} draggableId={el.moduleID}>
                                                             {(provided) => {
                                                                 return (
                                                                     <div
@@ -132,10 +176,11 @@ const ModuleExemptions = () => {
                                                                         {...provided.dragHandleProps}
                                                                     >
                                                                         <ModulePill
-                                                                            moduleID={el.id}
-                                                                            moduleCode={el.code}
-                                                                            moduleName={el.name}
-                                                                            moduleMC={el.mc}
+                                                                            moduleID={el.moduleID}
+                                                                            moduleCode={el.moduleCode}
+                                                                            moduleName={el.moduleName}
+                                                                            moduleMC={el.moduleMC}
+                                                                            moduleCategory={el.moduleCategory}
                                                                             yearSem="Exemptions"
                                                                             onClick={deleteModule}
                                                                         />
